19c24fc44e6f6747a87feb1c900ad9b5635a00a2
dralinz <dragon_lin95@163.com>
2020-08-23 21:07:33 +0800

    update

--- on_the_job/节点_过年/算法/leetcode/code/51.n皇后.js
+++ on_the_job/节点_过年/算法/leetcode/code/51.n皇后.js
@@ -10,15 +10,43 @@
  * @return {string[][]}
  */
 var solveNQueens = function(n) {
-    let def = function (res, path, n) {
-        if (path.length === n) {
-            res.push([])
+    let dfs = function (n, tmp, res) {
+        if (tmp.length === n) {
+            res.push(
+                tmp.map(i => {
+                    let arr = new Array(n).fill('.');
+                    arr[i] = 'Q';
+                    return arr.join('');
+                })
+            )
+            return
         }
-        for (let i = 0; i < n; i++) {
-            const element = n[i];
-            
+        // 当前的行数
+        let row = tmp.length;
+        for (let col = 0; col < n; col++) {
+            if (canPush(row, tmp, col)) {
+                tmp.push(col);
+                dfs(n, tmp, res);
+                tmp.pop();
+            }
+        }
+        function canPush(row, tmp, col) {
+            for (let index = 0; index < row; index++) {
+                let y = tmp[index];
+                if (y === col || index-y === row-col || index+y === row+col) {
+                    return false
+                }
+            }
+            // 不单单是一个左上,是整个斜行
+            // if (tmp[row -1] === col - 1 || tmp[row -1] === col + 1) {
+            //     return false
+            // }
+            return true
         }
     }
+    let res = [];
+    dfs(n, [], res);
+    return res;
 };
 // @lc code=end
 
