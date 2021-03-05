3e6622ecb43686f16e2c72bcc62921ab01db0e31
dralinz <dragon_lin95@163.com>
2021-03-05 20:34:18 +0800

    update

---
+++ on_the_job/21过年/函数源码/高阶函数.js
@@ -0,0 +1,19 @@
+// 一个函数就可以接收另一个函数作为参数，这种函数就称之为高阶函数。
+
+// 高阶函数简单举例
+
+function add (x, y, fn) {
+    return fn(x) + fn(y)
+}
+console.log(add (1, -1, Math.abs));
+
+function pow(x) {
+    return x * x;
+}
+
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// 平方
+arr.map(Math.pow); // [1, 4, 9, 16, 25, 36, 49, 64, 81]
+// 变为string
+arr.map(String); // ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+https://segmentfault.com/a/1190000012505900
