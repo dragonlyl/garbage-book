484acad4f126e054e0aeb2883519ef94b96b2adf
dralinz <dragon_lin95@163.com>
2021-04-27 22:09:33 +0800

    u

--- test.js
+++ test.js
@@ -1,13 +1,9 @@
-function test() {
-    let arr = [];
-    function tst (fn) {
-        return new Promise((resolve, reject) => {
-            // resolve(fn())
-            fn(resolve, reject);
-        })
-    }
-    for(let i = 0; i < arguments.length; i++) {
-        arr.push(tst(arguments[i]))
-    }
-    return Promise.all(arr)
+var a = 0
+var b = async () => {
+    console.log('3', a)
+  a = a + await 10
+  console.log('2', a) // -> ？
 }
+b()
+a++
+console.log('1', a) // -> ？
