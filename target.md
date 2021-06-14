42f14f81872ab832879d969bc3d5465cc879c3e4
dralinz <dragon_lin95@163.com>
2021-06-14 23:15:22 +0800

    update

--- target.md
+++ target.md
@@ -59,3 +59,115 @@ Array()是一个对象，[]是一个数据原型, 用new Array()系统每次都�
 ## 架构
 
 [如何一步步设计前端架构？](https://blog.csdn.net/weixin_44811417/article/details/93079579)
+
+## Map Set
+
+### Map
+
+map 的 `entries` 和 `keys`, `values` 返回的都是遍历器对象
+即 `map.keys().next()` 都是可以操作的
+
+map 的 `iterator` 就是 `entries()`函数
+
+`map[Symbol.iterator] === map.entries`
+
+WeakMap只接受对象为key值
+
+### WeakSet 和Set
+
+前者只能存对象(数组也算), 并且是弱引用,如果WeakSet内容没有被引用就会被垃圾处理回收
+所以**不能遍历**(因为里面元素数量取决于垃圾回收的处理时间),也没有`size`属性
+
+## node 垃圾回收
+
+```js
+// 运行能手动触发垃圾回收
+node --expose-gc
+
+// 先触发一次 回收
+> global.gc();
+// 查看内存状态
+> process.memoryUsage();
+{ rss: 21106688,
+  heapTotal: 7376896,
+  heapUsed: 4153936,
+  external: 9059 }
+
+> let wm = new WeakMap();
+
+// 新建一个变量 key，指向一个 5*1024*1024 的数组
+> let key = new Array(5 * 1024 * 1024);
+
+// 设置 WeakMap 实例的键名，也指向 key 数组
+// 这时，key 数组实际被引用了两次，
+// 变量 key 引用一次，WeakMap 的键名引用了第二次
+// 但是，WeakMap 是弱引用，对于引擎来说，引用计数还是1
+> wm.set(key, 1);
+> global.gc();
+// 这时内存占用 heapUsed 增加到 45M 了
+> process.memoryUsage();
+{ rss: 67538944,
+  heapTotal: 7376896,
+  heapUsed: 45782816,
+  external: 8945 }
+// 清除变量 key 对数组的引用，
+// 但没有手动清除 WeakMap 实例的键名对数组的引用
+> key = null;
+// 再次执行垃圾回收
+> global.gc();
+// 内存占用 heapUsed 变回 4M 左右，
+// 可以看到 WeakMap 的键名引用没有阻止 gc 对内存的回收
+> process.memoryUsage();
+```
+
+## 业务代码
+
+[业务代码究竟难不难写？](https://www.sohu.com/a/434687127_673711)
+
+> 再来看看文首的问题，你看，虽然只是写业务代码，如果要写的足够好，必须要了解设计模式、理解各种弹力设计、理解事务、熟悉框架、了解中间件原理，怎么可能学不到东西，要实现健壮的业务代码，其实很难，要考虑的东西太多了，如果说写框架我们需要考虑不同的使用方和使用环境，这很难，写业务代码我们要考虑到千奇百怪的使用行为，要考虑到层次不起的对接方，这不比写框架简单。对于5年+经验丰富的程序员应当有能力开一个好头，或者说愿意在老代码上去做一些改变，否则你的价值在哪里呢？
+
+## 前端 架构设计
+
+[前端架构设计的方法论](https://segmentfault.com/a/1190000016873522)
+
+## requestAnimationFrame (分片优化渲染)
+
+1. 请求动画帧 由系统绘制率(浏览器刷新频率)决定 (60Hz 1000/60 = 16.7ms)
+2. 会把每一帧的所有dom操作集中起来(再一次重绘或回流中完成)
+3. 隐藏或者不可见的元素不会重绘或回流
+
+[什么是requestAnimationFrame](https://zhuanlan.zhihu.com/p/171612962)
+[requestAnimationFrame详解](https://www.jianshu.com/p/fa5512dfb4f5)
+挂载在window上的方法.
+
+**它能保证回调函数在屏幕每一次的绘制间隔中只被执行一次, 这样就不会引起丢帧现象，也不会导致动画出现卡顿的问题。**
+
+跟`setTImeout`相比
+
+1. 时间较为固定,不会造成动画的卡顿
+
+## 渲染
+
+[渲染大量数据显示的优化](https://blog.csdn.net/alnorthword/article/details/87935038)
+
+主要问题
+
+1. 按照原来的做法，普通的组件，进行数据渲染插入dom中（几十条邹游），数据量小的时候很正常，vue很快。
+2. 改为分段实现，把一批数据进行分块，通过定时器取出相应的部分去渲染，这种做法，比第一中稍微好点
+3. 改为分段+虚拟DOM实现，这种做法，当达到几百条就会出现问题。
+
+解决思路
+
+1. 不要渲染全部数据，只渲染用户眼睛能看到的部分数据。
+2. 不要用Vue去管理和监听全部的数据，造成很大的内存开销，可以自己定义个变量计算。
+
+### 大数据处理
+
+前端分片优化渲染
+[前端页面如何正确渲染大量数据](https://blog.csdn.net/yy168888/article/details/115660601?utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromMachineLearnPai2%7Edefault-3.control&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromMachineLearnPai2%7Edefault-3.control)
+
+### 微任务 queueMicrotask
+
+宏任务 macro-task;
+
+## 千分位
--- test.js
+++ test.js
@@ -1,34 +1,226 @@
 
-Function.prototype.myApply = function (context) {
-    if (typeof this !== 'function') {
-        throw new TypeError('not function')
+// [深入分析 Promise 实现细节](https://juejin.cn/post/6945319439772434469#heading-17)
+// [BAT前端经典面试问题：史上最最最详细的手写Promise教程](https://juejin.cn/post/6844903625769091079#heading-10)
+class MyPromise {
+    constructor(fn) {
+        try {
+            
+            this.status = 'pending';
+            this.result = null;
+            this.reason = null;
+            fn(this.resolve, this.reject);
+            this.successCallback = [];
+            this.failCallback = [];
+        } catch (error) {
+            this.reject(error)            
+        }
+
     }
-    let _this = this || window;
-    let fn = Symbol();
-    context[fn] = _this;
-    let result;
-    if (arguments[1]) {
-        result = context[fn](...arguments[1])
-    } else {
-        result = context[fn]()
+    resolve = (res) =>  {
+        if (this.status === 'pending') {
+            this.status = 'resolve'
+            this.result = res;
+            while (this.successCallback && this.successCallback.length) {
+                let fn = this.successCallback.shift()
+                console.log('异步方法')
+                fn()
+            }
+        }
     }
-    delete context[fn];
-    return result;
+    reject = (value) => {
+        if (this.status === 'pending') {
+            this.status = 'reject'
+            this.reason = value;
+            while(this.failCallback && this.failCallback.length) {
+                this.failCallback.shift()(value)
+            }
+        }
+    }
+    // then(onFulfilled, onRejected) {
+    //     // 还没有返回 缓存一下函数
+    //     if (this.status === 'pending') {
+    //         if (onFulfilled) {
+    //             this.successCallback.push(onFulfilled);
+    //         }
+    //         if (onRejected) {
+    //             this.failCallback.push(onRejected);
+    //         }
+    //     }
+    //     if (this.status === 'resolve') {
+    //         console.log('同步方法')
+    //         onFulfilled(this.result)
+    //     }
+    //     if (this.status === 'reject') {
+    //         onRejected(this.reason)
+    //     }
+    // }
+    then (onFulfilled, onRejected) {
+        onFulfilled = typeof onFulfilled !== 'function' ? (v) => v : onFulfilled;
+        function resolvePromise(promise1, x, resolve, reject) {
+            if (x === promise1) {
+                return reject(new TypeError('promise same'))
+            }
+            if (x instanceof MyPromise) {
+                x.then(resolve, reject)
+            } else {
+                resolve(x)
+            }
+        }
+        let promise = new MyPromise((resolve, reject) => {
+            const fulFilledMicrotask = () => {
+                queueMicrotask(() => {
+                    try {
+                        const x = onFulfilled(this.result);
+                        resolvePromise(promise, x, resolve, reject);
+                    } catch (error) {
+                        reject(error)
+                    }
+                })
+            }
+            const rejectedMicrotask = (() => {
+                queueMicrotask(() => {
+                    try {
+                        let x =  onRejected(this.reason);
+                        resolvePromise(promise, x, resolve, reject)
+                    } catch (error) {
+                        reject(error);
+                    }
+                })
+            })
+            if (this.status === 'pending') {
+                this.successCallback.push(fulFilledMicrotask);
+                this.failCallback.push(onRejected);
+            }
+            if (this.status === 'resolve') {
+                fulFilledMicrotask();
+            }
+            if (this.status === 'reject') {
+                rejectedMicrotask();
+            }
+        })
+        return promise;
+    }
+
 }
 
-let obj = {t: '33'}
-function tt(a,bb) {
-    console.log(this.t, a,bb);
+let promise = new MyPromise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('333')
+    }, 1000)
+})
+// promise.then(value => {
+//     console.log(1)
+//     console.log('resolve', value)
+// })
+// promise.then(value => {
+//     console.log(2)
+//     console.log('resolve', value)
+// })
+// promise.then(value => {
+//     console.log(3)
+//     console.log('resolve', value)
+// })
+function other () {
+    return new MyPromise((resolve, reject) =>{
+      resolve('other')
+    })
 }
-tt(12,22);
-tt.myApply(obj,[12,33])
-Function.prototype.myBind = function () {
-    let context = [].shift.call(arguments) || window;
-    let arg = arguments;
-    let fn = this;
-    return function fn1 () {
-        return this instanceof fn1 ? new fn1(...arguments) : fn.apply(context, arg.concat(...arguments))
+promise.then(value => {
+    console.log(4);
+    return other()
+}).then(res => {
+    console.log('other', res)
+})
+
+function coinSelect(coins, num) {
+    let arr = Array(num + 1).fill(num + 1);
+    arr[0] = 0;
+    for (let i = 1; i <= num; i++) {
+        for (let j = 0; j < coins.length; j++) {
+            if (i - coins[j] >= 0) {
+                arr[i] = Math.min(arr[i], arr[i - coins[j]] + 1);
+            }
+        }
     }
+    if (arr[num] === num + 1) {
+        return -1
+    }
+    return arr[num]
 }
-let fn1 = tt.bind(obj);
-fn1(12,22)
+// console.log(coinSelect([1, 2, 5], 11))
+
+const getJson = function (url) {
+    return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest()
+        xhr.open('get', url, false);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState !== 4) {
+                return
+            }
+            if (xhr.status === 200) {
+                resolve(xhr.responseText)
+            } else {
+                reject(new Error(xhr.responseText))
+            }
+        }
+        xhr.send();
+    })
+}
+
+const obj = {
+    a: {
+        b: 1,
+        c: 2,
+        d: {e: 5}
+    },
+    b: [1, 3, {a: 2, b: 3}],
+    c: 3
+}
+function flatten(flattenObj) {
+    let retObj = {}
+    // 是个对象
+    let dfs = (obj, pre) => {
+        if (typeof obj === 'object') {
+            let keys = Object.keys(obj);
+            let bArray = Array.isArray(obj);
+            keys.forEach(val => {
+                let str = val
+                if (pre) {
+                    str = bArray ?(`${pre}[${val}]`) : (pre + '.' + val);
+                }
+                dfs(obj[val], str);
+            })
+        } else {
+            retObj[pre] = obj
+        }
+    }
+    dfs(flattenObj, '');
+    return retObj;    
+}
+// console.log(flatten(obj));
+// {
+//  'a.b': 1,
+//  'a.c': 2,
+//  'a.d.e': 5,
+//  'b[0]': 1,
+//  'b[1]': 3,
+//  'b[2].a': 2,
+//  'b[2].b': 3
+//   c: 3
+// }
+
+let template = '我是{{name}}，年龄{{age}}，性别{{sex}}';
+let data = {
+  name: '姓名',
+  age: 18
+}
+console.log(render(template, data)); // 我是姓名，年龄18，性别undefined
+
+function render(t, d) {
+    // 匹配到的字符  , 捕获到的字符
+    return t.replace(/\{\{(\w+)\}\}/g, (match, val) => {
+        console.log(match, val)
+        return d[val];
+    })
+}
