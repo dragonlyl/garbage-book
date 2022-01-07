ddfb4b6e115807bdd4558eb2b212aafba73564cf
linyuelong <linyuelong@youzan.com>
2022-01-07 10:47:07 +0800

    feat: 英语和源码

--- new1.js
+++ new1.js
@@ -35,3 +35,21 @@ function BinarySearchTree() {
 	};
 
 };
+
+
+const url = 'https://www.runoob.com/wp-content/themes/runoob/option/alisearch/v330/hot_hint.php?type=hint&user_id=b0d9a905-ab68-4044-abca-9b05e249801a'
+function test (ctx) {
+  return new Promise(async resolve => {
+      setTimeout(() => {
+          resolve(2)
+      },1000)
+    // let res = await ctx.ajax({url})
+    // resolve(res)
+  })
+}
+
+console.log(test().then(res => {console.log(res ,'res')}))
+function main(ctx, utils) {
+  return {
+  }
+}
