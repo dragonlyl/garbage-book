c841ecef9b944159f177f3e5159b05ba8e8f10bc
linyuelong <linyuelong@youzan.com>
2022-01-23 15:36:58 +0800

    feat: 添加commitizen

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,21 +2,21 @@ module.exports = {
     extends: ['@commitlint/config-conventional'],
     // 检测规则
     rules: {
-        'type-enum': [
-        2,
-        'always',
-        [
-            'feat',
-            'fix',
-            'docs',
-            'style',
-            'refactor',
-            'perf',
-            'test',
-            'chore',
-            'revert',
-            'build'
-        ]
+        'type-enum': [ 
+            2,
+            'always',
+            [
+                'feat',
+                'fix',
+                'docs',
+                'style',
+                'refactor',
+                'perf',
+                'test',
+                'chore',
+                'revert',
+                'build'
+            ]
         ],
         'type-case': [0],
         'type-empty': [0],
