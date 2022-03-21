ff357a323244521d923c1689b33b59266174f04b
linyuelong <linyuelong@youzan.com>
2022-03-21 10:26:57 +0800

    feat: 组件库

--- typescript/ts编程.ts
+++ typescript/ts编程.ts
@@ -7,7 +7,7 @@ const obj = {
   };
 // 获取已经存在的变量类型 通过 typeof
 type Person = typeof obj;
-type PersonTest = keyof obj; // 
+type PersonTest = keyof obj;
 type PersonTest1 = keyof Person; // 取 interface 键
 // 等价于
 type Person1 = {
@@ -89,16 +89,50 @@ const a1 = get(obj, 'age')
 
 /**
  * [映射类型](https://juejin.cn/post/6844904169929703437)
+ * 用于重用和修改已存在的类型
  */
 
  interface Person {
     name: string;
     age: number;
   }
-  
+const Person1 = {
+    name: 'tst',
+    age: 12
+}
   interface OptionalPerson1 {
     name?: string;
     age?: number;
   }
-  
+// 多了一个 in
 type OptionalPerson = { [K in keyof Person]?: Person[K] };
+type OptionalPerson2 = { [K in keyof Person1]?: Person[K] };
+type OptionalPerson3 = { [K in keyof typeof Person1]?: Person[K] };
+
+
+// + - 操作符
+
+// 通过 - 号来去除 ? ; 同样  -readonly 也能减掉 readonly
+type Person2 = { [K in keyof OptionalPerson1]-?: Person[K] };
+
+
+// 深层映射 (多级嵌套)
+
+interface DeepObj {
+    name: string;
+    age: number;
+    deepAttributes: {
+      prop1: string;
+      prop2: string;
+      obj: {
+        other: number;
+      };
+    };
+  }
+  
+  type DeepPartial<T> = {
+    [K in keyof T]?: T[K] extends object ? DeepPartial<T[K]> : T[K];
+  };
+  
+  type PartialDeepOjb = DeepPartial<DeepObj>;
+ 
